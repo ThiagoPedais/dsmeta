85a0fa0aa82923d881454cc3891855e340957d8e
Thiago Pedais <luizthiago20sccp@gmail.com>
2022-07-27 19:30:41 -0300

    Axios, useEffect

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import NotificationButton from '../NotificationButton';
@@ -14,6 +15,12 @@ const SalesCard = () => {
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
 
+    useEffect(() => {
+        axios.get("http://localhost:8080/sales")
+        .then(res => {
+            console.log(res.data)
+        });
+    }, [])
 
 
 
